5b6f54ec49802dcf1f87a35fe7a4434a55802085
Nyamador <dselasi4u@gmail.com>
2020-12-02 15:08:11 +0000

    Added Restaurant API list view

--- inventory/menus/views.py
+++ inventory/menus/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Restaurant
+from .serializers import RestaurantSerializer
 
 # Create your views here.
+class RestaurantList(generics.ListAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer

b595b33dfc4039e6e86b0cfaf941f98825c747ba
Nyamador <dselasi4u@gmail.com>
2020-12-15 14:11:51 +0000

    Added Models to admin

--- inventory/menus/admin.py
+++ inventory/menus/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from . models import Restaurant
+from . models import Restaurant, Food, Category
 
 # Register your models here.
 admin.site.register(Restaurant)
+admin.site.register(Food)
+admin.site.register(Category)

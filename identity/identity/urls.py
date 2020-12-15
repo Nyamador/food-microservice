ba86c847c8bf0f8dd4b88d8d340da7733a3ba563
Nyamador <dselasi4u@gmail.com>
2020-12-15 13:35:02 +0000

    Added route for jwt verification

--- identity/identity/urls.py
+++ identity/identity/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework_jwt.views import verify_jwt_token
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-token-verify/', verify_jwt_token),
 ]

ac8e846477b23509c37ffda837d12bc5d41e7fd3
Nyamador <dselasi4u@gmail.com>
2020-12-17 11:21:44 +0000

    Feat: Changed jwt routes to simple_jwt package routes

--- identity/identity/urls.py
+++ identity/identity/urls.py
@@ -16,11 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from rest_framework_jwt.views import verify_jwt_token, obtain_jwt_token, refresh_jwt_token
+# from rest_framework_jwt.views import verify_jwt_token, obtain_jwt_token, refresh_jwt_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-token-auth/', obtain_jwt_token),
-    path('api-token-refresh/', refresh_jwt_token),
-    path('api-token-verify/', verify_jwt_token),
+    # path('api-token-auth/', obtain_jwt_token),
+    # path('api-token-refresh/', refresh_jwt_token),
+    # path('api-token-verify/', verify_jwt_token),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify' ),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_resfresh'),
 ]

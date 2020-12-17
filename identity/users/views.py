10c81cbe02a042f5de81efe3fff1f29d79d115fd
Nyamador <dselasi4u@gmail.com>
2020-12-17 12:22:35 +0000

    Added protected view

--- identity/users/views.py
+++ identity/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.decorators import  api_view
+from rest_framework.views import Response
 
 # Create your views here.
+@api_view()
+def some_protected_view(request):
+    return Response({"message": "Hello for today! See you tomorrow!"})

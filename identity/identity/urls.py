e7ccf7319b7e520566b0dc23b67cbabfbf487d7f
Nyamador <dselasi4u@gmail.com>
2020-12-18 12:01:39 +0000

    Fix: Corrected typo in refresh token endpoint namespace

--- identity/identity/urls.py
+++ identity/identity/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     path('', some_protected_view, name="protected_view"),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify' ),
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token_resfresh'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
